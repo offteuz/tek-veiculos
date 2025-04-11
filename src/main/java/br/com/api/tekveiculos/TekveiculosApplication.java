1c671aa23bd5a8bdd29f4b9f7961190b413f556b
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-04-11 18:02:48 -0300

    first commit.

---
+++ src/main/java/br/com/api/tekveiculos/TekveiculosApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.tekveiculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekveiculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekveiculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/tekveiculos/TekveiculosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.tekveiculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekveiculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
