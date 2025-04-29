40e0d158c4d488a922245a0795c9b580b569c371
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-04-29 05:22:16 -0300

    implementation of the "status" class.

--- src/main/java/br/com/api/tekveiculos/model/Status.java
+++ src/main/java/br/com/api/tekveiculos/model/Status.java
@@ -1,4 +1,26 @@
 package br.com.api.tekveiculos.model;
 
+import jakarta.persistence.*;
+import jakarta.validation.Valid;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity(name = "STATUS")
 public class Status {
+
+    @Id
+    @SequenceGenerator(name = "seq_status", sequenceName = "seq_status", allocationSize = 1)
+    @GeneratedValue(generator = "seq_status", strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    private String description;
+
+    @Valid
+    @Embedded
+    private Audit audit;
 }
