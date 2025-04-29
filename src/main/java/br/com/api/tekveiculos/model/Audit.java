ceb28fdf92f9c9ac876fdc46e47c3499877c8482
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-04-29 05:23:07 -0300

    implementation of the "audit" class.

--- src/main/java/br/com/api/tekveiculos/model/Audit.java
+++ src/main/java/br/com/api/tekveiculos/model/Audit.java
@@ -1,4 +1,30 @@
 package br.com.api.tekveiculos.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Embeddable
 public class Audit {
+
+    @CreationTimestamp
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @JsonFormat(pattern = "dd-MM-yyyy HH:mm:ss")
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
 }
