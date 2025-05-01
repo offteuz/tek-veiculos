202a84a43af3398802947cb7b663bb1373083f62
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-05-01 13:44:08 -0300

    creation and implementation of the "Client" class.

---
+++ src/main/java/br/com/api/tekveiculos/model/Client.java
@@ -0,0 +1,38 @@
+package br.com.api.tekveiculos.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Entity(name = "CLIENT")
+public class Client {
+
+    @Id
+    @SequenceGenerator(name = "seq_client", sequenceName = "seq_client", allocationSize = 1)
+    @GeneratedValue(generator = "seq_client", strategy = GenerationType.SEQUENCE)
+    private Long id;
+
+    @Column(name = "social_reason")
+    private String socialReason;
+
+    @Column(name = "fantasy_name")
+    private String fantasyName;
+
+    private String city;
+
+    private String uf;
+
+    @ManyToOne
+    @JoinColumn(name = "status_id", referencedColumnName = "id")
+    @JsonIgnore
+    private Status status;
+
+    @Embedded
+    private Audit audit;
+}
