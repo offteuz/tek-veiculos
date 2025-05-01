a8cb183f68d7fa574e0ca03e57642a97f384260b
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-05-01 13:47:23 -0300

    creation and implementation of the "clientController" (create) class.

---
+++ src/main/java/br/com/api/tekveiculos/controller/ClientController.java
@@ -0,0 +1,25 @@
+package br.com.api.tekveiculos.controller;
+
+import br.com.api.tekveiculos.dto.request.ClientRequestDTO;
+import br.com.api.tekveiculos.dto.response.ClientResponseDTO;
+import br.com.api.tekveiculos.service.ClientService;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class ClientController {
+
+    private final ClientService service;
+
+    public ClientController(ClientService service) {
+        this.service = service;
+    }
+
+    @PostMapping("/create/client")
+    @ResponseStatus(HttpStatus.OK)
+    private ClientResponseDTO create(@Valid @RequestBody ClientRequestDTO dto) {
+        return service.create(dto);
+    }
+}
