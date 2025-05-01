f56dbb96340497e8959cd56d1657e40271a27beb
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-05-01 13:48:00 -0300

    creation and implementation of the "clientMapper" class, responsible for mapping between model classes and DTOs.

---
+++ src/main/java/br/com/api/tekveiculos/mapper/ClientMapper.java
@@ -0,0 +1,25 @@
+package br.com.api.tekveiculos.mapper;
+
+import br.com.api.tekveiculos.dto.request.ClientRequestDTO;
+import br.com.api.tekveiculos.dto.response.ClientResponseDTO;
+import br.com.api.tekveiculos.model.Client;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring", uses = {StatusMapperHelper.class}, unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface ClientMapper {
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "audit", ignore = true)
+    @Mapping(source = "statusId", target = "status")
+    Client toDTO(ClientRequestDTO dto);
+
+    ClientResponseDTO toModel(Client client);
+
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "audit", ignore = true)
+    @Mapping(source = "statusId", target = "status")
+    void update(ClientRequestDTO dto, @MappingTarget Client client);
+}
