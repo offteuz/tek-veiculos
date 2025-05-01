555616155652e573a1ce64911169ce04eaee05d1
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-05-01 13:49:52 -0300

    creation and implementation of the "statusMapperHelper" class.

---
+++ src/main/java/br/com/api/tekveiculos/mapper/StatusMapperHelper.java
@@ -0,0 +1,21 @@
+package br.com.api.tekveiculos.mapper;
+
+import br.com.api.tekveiculos.exception.StatusNotFoundException;
+import br.com.api.tekveiculos.model.Status;
+import br.com.api.tekveiculos.repository.StatusRepository;
+import org.springframework.stereotype.Component;
+
+@Component
+public class StatusMapperHelper {
+
+    private final StatusRepository statusRepository;
+
+    public StatusMapperHelper(StatusRepository statusRepository) {
+        this.statusRepository = statusRepository;
+    }
+
+    public Status map(Long statusId) {
+        return statusRepository.findById(statusId)
+                .orElseThrow(() -> new StatusNotFoundException());
+    }
+}
