392de5002689cb5bd63ae209a0f0a8ec89ff002a
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-05-19 10:12:13 -0300

    implementation of the remaining methods for the CRUD of the "ClientController" class, which are: Find all, find by id, update, and delete.

--- src/main/java/br/com/api/tekveiculos/controller/ClientController.java
+++ src/main/java/br/com/api/tekveiculos/controller/ClientController.java
@@ -7,6 +7,8 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api")
 public class ClientController {
@@ -21,5 +23,29 @@ public class ClientController {
     @ResponseStatus(HttpStatus.CREATED)
     private ClientResponseDTO create(@Valid @RequestBody ClientRequestDTO dto) {
         return service.create(dto);
+    }
+
+    @GetMapping("/find-all/client")
+    @ResponseStatus(HttpStatus.OK)
+    private List<ClientResponseDTO> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/find-by-id/client/{idClient}")
+    @ResponseStatus(HttpStatus.OK)
+    private ClientResponseDTO findById(@PathVariable Long idClient) {
+        return service.findById(idClient);
+    }
+
+    @PutMapping("/update/client/{idClient}")
+    @ResponseStatus(HttpStatus.OK)
+    private ClientResponseDTO update(@PathVariable Long idClient, @Valid @RequestBody ClientRequestDTO dto) {
+        return service.update(idClient, dto);
+    }
+
+    @DeleteMapping("/delete/client/{idClient}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    private void delete(@PathVariable Long idClient) {
+        service.delete(idClient);
     }
 }
