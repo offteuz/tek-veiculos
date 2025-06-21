f433374dee5d6f73716b95938911b3b9e3020644
Matheus Jesus de Souza <matheusxone47@gmail.com>
2025-06-21 19:19:40 -0300

    Implementation of mappings related to the "vehicle" entity, being with the entities: "status" and "visitClient".

--- src/main/java/br/com/api/tekveiculos/model/Vehicle.java
+++ src/main/java/br/com/api/tekveiculos/model/Vehicle.java
@@ -4,9 +4,12 @@ import br.com.api.tekveiculos.model.enumerator.MarkVehicle;
 import br.com.api.tekveiculos.model.enumerator.ModelVehicle;
 import br.com.api.tekveiculos.model.enumerator.TypeVehicle;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import jakarta.validation.Valid;
 import lombok.*;
+
+import java.util.List;
 
 @Getter
 @Setter
@@ -41,10 +44,16 @@ public class Vehicle {
 
     private String km;
 
-    @ManyToOne
-    @JoinColumn(name = "status_id", referencedColumnName = "id")
     @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "status_id", referencedColumnName = "id", foreignKey = @ForeignKey(name = "fk_vehicle_status"))
     private Status status;
+
+    @JsonIgnore
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "vehicle", fetch = FetchType.LAZY)
+    private List<VisitClient> visitClientLists;
 
     @Valid
     @Embedded
